5ac6b1ba20c03172533c5b20abaf0d094a43cfb9
linkai <49516283+Link-Go@users.noreply.github.com>
2021-05-17 12:23:54 +0800

    Update access_log.py

--- access_log.py
+++ access_log.py
@@ -30,6 +30,27 @@ class NewRoute(APIRoute):
                     return Match.FULL, child_scope
         return Match.NONE, {}
 
+    def scope_headers(self, scope: Scope) -> dict:
+        if not scope.get("headers"):
+            return {}
+
+        return {key.decode(): value.decode() for key, value in dict(scope["headers"]).items()}
+
+    def scope_cookie(self, scope: Scope) -> dict:
+        headers = self.scope_headers(scope)
+        cookie_dict = {}
+        for chunk in headers.get("cookie", "").split(";"):
+            if "=" in chunk:
+                key, val = chunk.split("=", 1)
+            else:
+                # Assume an empty name per
+                key, val = "", chunk
+            key, val = key.strip(), val.strip()
+            if key or val:
+                cookie_dict[key] = val
+        return cookie_dict
+
+    
 
 r = APIRouter(route_class=NewRoute)
 
